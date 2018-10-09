b5394ae14b92b75454da7ad7724fb9adc4ac16c1
cleiton <cleiton.junior@unigranrio.br>
2018-10-09 19:59:57 -0300

    Merge com o branch logic-blocks

---
+++ libs/blockly/blocks/custom/move_blocks.js
@@ -0,0 +1,15 @@
+Blockly.JavaScript["controls_moveUp"] = function(block){
+    return 'moveUp();\n';
+}
+
+Blockly.JavaScript["controls_moveDown"] = function(block){
+    return 'moveDown();\n';
+}
+
+Blockly.JavaScript["controls_moveLeft"] = function(block){
+    return 'moveLeft();\n';
+}
+
+Blockly.JavaScript["controls_moveRight"] = function(block){
+    return 'moveRight();\n';
+}
